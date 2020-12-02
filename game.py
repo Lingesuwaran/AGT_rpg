c0a1f3312324792edc6ff52c436117ce1b14aefe
Lingesuwaran <46320222+Lingesuwaran@users.noreply.github.com>
2020-12-02 19:51:11 +0530

    Update game.py

--- game.py
+++ game.py
@@ -4,29 +4,54 @@ from colorama import Fore, Back, Style  #Fore: BLACK, RED, GREEN, YELLOW, BLUE, 
                                         #Style: DIM, NORMAL, BRIGHT, RESET_ALL
 import gpt_2_simple as gpt2
 
-world = "world"
 def Play(a):
+
+  file_name = "/content/AGT_rpg/Books/data.txt"
+
+  l=pyfiglet.figlet_format("Loading...", font = "slant")
+  g=pyfiglet.figlet_format("Generating world...", font = "slant")
+  print(Fore.GREEN)
+
+  gpt2.download_gpt2(model_name="774M")
+  print(Style.BRIGHT+g)
   sess = gpt2.start_tf_sess()
-  gpt2.generate(sess,
-              length=250,
-              temperature=0.7,
-              prefix="%Name%",
-              nsamples=5,
-              batch_size=5,
-              top_k=40,
-              run_name=world
-              )
+
+  gpt2.finetune(
+      sess,
+      dataset  = file_name,
+      model_name = '774M',
+      steps=1000,
+      restore_from='fresh',
+      run_name = 'run1',
+      print_every = 1000,
+      sample_every = 5000,
+      save_every = 100         
+                )
+
+  print(Style.BRIGHT+l)
+  print(Fore.RESET)
+  input1=""
+  while input1 != 'esc':
+    input1 = input("Enter something or esc to exit")
+    gpt2.generate(sess,
+                length=250,
+                temperature=0.7,
+                prefix=input1,
+                nsamples=5,
+                batch_size=5,
+                top_k=40,
+                run_name='run1'
+                )
 
 
 
 
 if __name__ == "__main__":
-  print(Fore.MAGENTA + 'some red text')
+  print(Fore.MAGENTA)
   #print(Back.BLACK)
   result1 = pyfiglet.figlet_format("AGT", font = "isometric1") 
   result2 = pyfiglet.figlet_format(" rpg", font = "slant")
   print(Style.BRIGHT+result1+result2) 
-  from colorama import Fore, Back, Style
   #Menu
   print("1. Play \n2. Load \n3.Exit")
   while True:
