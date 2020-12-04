baf6a176dda98c15749a5bd43c28b2ac7d193986
Lingesuwaran <46320222+Lingesuwaran@users.noreply.github.com>
2020-12-05 03:04:26 +0530

    Update preprossesing.py

--- preprossesing.py
+++ preprossesing.py
@@ -1,10 +1,64 @@
 import fitz
+import ebooklib
+from ebooklib import epub
+from bs4 import BeautifulSoup
+
+blacklist = [   '[document]',   'noscript', 'header',   'html', 'meta', 'head','input', 'script', 'style'  ]
+
 file_name=input("Enter the file name with extention")
-pdf=fitz.open("/content/AGT_rpg/Books/"+file_name)
-n=pdf.pageCount
-for i in range(n):
-  page = pdf.loadPage(i)
-  text = page.getText('text')
+file_ext = file_name.split('.')
+
+
+
+def	epub2html(epub_path):
+	book = epub.read_epub(epub_path)
+	chapters = []
+	for i in book.get_items():
+		if i.get_type() == ebooklib.ITEM_DOCUMENT:
+			chapters.append(i.get_content())
+	return chapters
+
+def chap2text(chap):
+	output = ''
+	soup = BeautifulSoup(chap,'html.parser')
+	text = soup.find_all(text=True)
+	for t in text:
+		if t.parent.name not in blacklist:
+			output += '{}'.format(t)
+	return output
+
+def thtml2ttext(thtml):
+  Output = []
+  for html in thtml:
+    text =  chap2text(html)
+    Output.append(text)
+  return Output
+
+def epub2text(epub_path):
+  chapters = epub2html(epub_path)
+  ttext = thtml2ttext(chapters)
+  return ttext
+
+
+if file_ext[1] == 'pdf':
+  pdf=fitz.open("/content/AGT_rpg/Books/"+file_name)
+  n=pdf.pageCount
+  for i in range(n):
+    page = pdf.loadPage(i)
+    text = page.getText('text')
+    with open('/content/AGT_rpg/Books/data.txt','a')as file:
+      file.write(text.replace('\t',' '))
+  pdf.close()
+elif file_ext[1] == 'epub':
+  text_L = epub2text("/content/AGT_rpg/Books/"+file_name)
+  text = ''''''
+  for i in text_L:
+    text += i
+  text = text.replace('\n',' ')
+  text = text.replace('”','')
+  text = text.replace('“','')
   with open('/content/AGT_rpg/Books/data.txt','a')as file:
-	  file.write(text.replace('\t',''))
-pdf.close()
+      file.write(text)
+else:
+  print('invalid')
+
