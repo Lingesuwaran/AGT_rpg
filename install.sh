d86ef99359485d1efc68bbb6c8f24e8071ee1576
Lingesuwaran <46320222+Lingesuwaran@users.noreply.github.com>
2020-12-09 12:53:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-C8AfjQVJIS5xMgzo4tx0miFfof8PPVa' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-C8AfjQVJIS5xMgzo4tx0miFfof8PPVa" -O checkpoint.zip && rm -rf /tmp/cookies.txt
+#wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-C8AfjQVJIS5xMgzo4tx0miFfof8PPVa' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-C8AfjQVJIS5xMgzo4tx0miFfof8PPVa" -O checkpoint.zip && rm -rf /tmp/cookies.txt
 unzip checkpoint.zip
 rm checkpoint.zip
 pip install gpt-2-simple
