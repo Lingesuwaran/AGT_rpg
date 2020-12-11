75ddd404d084e4769301f204a56432fb79f47a98
Lingesuwaran <46320222+Lingesuwaran@users.noreply.github.com>
2020-12-11 10:30:15 +0530

    Updating GUI

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask_ngrok import run_with_ngrok
+from flask import Flask, render_template,request
+from AGT_rpg import game
+
+
+app = Flask(__name__)
+run_with_ngrok(app)
+
+@app.route('/')
+def Title_screen():
+  return render_template('Title_screen.html')
+
+@app.route('/Play',methods= ["POST",'GET'])
+def Play():
+  data = ""
+  if request.method == "POST":
+    input1 = request.form["input"]
+    print(input1)
+    data = game.Menu(1,str(data+" "+input1))
+    return render_template('Play.html',data=data)
+  else:
+    data = game.Menu(1,1)
+    return render_template('Play.html',data=data)
+
+
+app.run()
