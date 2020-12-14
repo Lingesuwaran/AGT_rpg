5d54a4f316343b1d9d967e0f06094edcfa6a66e9
Lingesuwaran <46320222+Lingesuwaran@users.noreply.github.com>
2020-12-14 19:05:08 +0530

    Update install.sh

--- game/install.sh
+++ game/install.sh
@@ -1,5 +1,4 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1QeSXSE88AaMta7V123qVpO6H43DNywaX' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1N7LVLbxuL67_irmuSmJp_WRmSDHxanu5" -O checkpoint.zip && rm -rf /tmp/cookies.txt
-pip install gpt-2-simple
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1N7LVLbxuL67_irmuSmJp_WRmSDHxanu5' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1N7LVLbxuL67_irmuSmJp_WRmSDHxanu5" -O checkpoint.zip && rm -rf /tmp/cookies.txtpip install gpt-2-simple
 unzip checkpoint.zip -d /content/AGT_rpg/
 rm checkpoint.zip
 pip install Pillow==2.2.1
